be9e316aa77b8af1591d9554cf1428fac9d29cf1
Luis Benitez <luisbttf@gmail.com>
2018-07-18 12:40:20 -0500

    resp

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -367,7 +367,30 @@ $( document ).ready(function() {
     speed: 300,
     slidesToShow: 1,
     centerMode: true,
-    variableWidth: true
+    variableWidth: true,
+    centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    }
+  ]
   });
 
 
